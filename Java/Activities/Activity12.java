650ff03a9bd3269c812ce2a9603e7b651f0e641f
Saravanan K <Saravanan.k1@ibm.com>
2024-05-13 19:15:46 +0530

    Add files via upload

---
+++ Java/Activities/Activity12.java
@@ -0,0 +1,24 @@
+package org.example;
+
+interface Addable {
+    int add(int a, int b);
+}
+
+interface Addable {ad2 = (int a = 0, int b) -> {
+    return (a + b);
+};
+
+public class Activity12 {
+    public static void main(String[] args) {
+
+        // Lambda expression without return keyword.
+        Addable ad1 = (a, b) -> (a + b);
+        System.out.println(ad1.add(10, 20));
+
+        // Lambda expression with return keyword.
+        Addable ad2 = (int a, int b) -> {
+            return (a + b);
+        };
+        System.out.println(ad2.add(100, 200));
+    }
+}
